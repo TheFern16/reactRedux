0dff7225b3738d29ad598413560deaa97a8f4aca
woonandez <matthew.allen.fernandez@gmail.com>
2017-09-28 11:26:50 -0700

    set up jsx and add watch command to watch script

--- js/myTitle.js
+++ js/myTitle.js
@@ -5,9 +5,12 @@ var h1 = React.DOM.h1;
 
 var MyTitle = React.createClass({
   render: function() {
+    const style = { color: this.props.color }
     return (
-      div(null, h1({style: {color: this.props.color } }, this.props.title)
-      )
+      <div>
+        <h1 style={ style }></h1>
+          { this.props.title }
+      </div>
     )
   }
 });
